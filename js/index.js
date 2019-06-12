5cad24a5b9d41a4c0a49973667ce11a6c1988928
Aaditya Deshpande <41305804+AadityaDeshpande@users.noreply.github.com>
2019-06-12 23:39:04 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/*var res= document.getElementById("resume")
+res.addEventListener("click",resume);
+function resume(){
+  alert("you clicked my resume");
+	//window.open("media/Resume_job.pdf","_self");
+}*/
